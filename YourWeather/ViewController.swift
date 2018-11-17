ed1f0ecc3a4c257b4308e1a1f3f6b7ad2bd5ab19
AlexanderKogut <alexander.kogyt@gmail.com>
2018-11-17 13:14:23 +0200

    Initial Commit

---
+++ YourWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourWeather
+//
+//  Created by AlexanderKogut on 11/17/18.
+//  Copyright © 2018 AlexanderKogut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
